0d40b3f0497918d81baa77dfc83b330392605269
Shripal Jain <shripal17@gmail.com>
2023-05-23 21:43:22 +0200

    Use monet theme
    Signed-off-by: Shripal Jain <shripal.jain@leanix.net>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,14 +8,58 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  final _appPrimaryColor = Colors.deepPurple;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightColorScheme;
+        ColorScheme darkColorScheme;
+
+        if (lightDynamic != null && darkDynamic != null) {
+          // On Android S+ devices, use the provided dynamic color scheme.
+          // (Recommended) Harmonize the dynamic color scheme' built-in semantic colors.
+          lightColorScheme = lightDynamic.harmonized();
+          // (Optional) Customize the scheme as desired. For example, one might
+          // want to use a brand color to override the dynamic [ColorScheme.secondary].
+          lightColorScheme = lightColorScheme.copyWith(secondary: _appPrimaryColor);
+          // (Optional) If applicable, harmonize custom colors.
+          // lightCustomColors = lightCustomColors.harmonized(lightColorScheme);
+
+          // Repeat for the dark color scheme.
+          darkColorScheme = darkDynamic.harmonized();
+          darkColorScheme = darkColorScheme.copyWith(secondary: _appPrimaryColor);
+          // darkCustomColors = darkCustomColors.harmonized(darkColorScheme);
+
+          // _isDemoUsingDynamicColors = true; // ignore, only for demo purposes
+        } else {
+          // Otherwise, use fallback schemes.
+          lightColorScheme = ColorScheme.fromSeed(
+            seedColor: _appPrimaryColor,
+          );
+          darkColorScheme = ColorScheme.fromSeed(
+            seedColor: _appPrimaryColor,
+            brightness: Brightness.dark,
+          );
+        }
+
+        return MaterialApp(
+
+          theme: ThemeData(
+            colorScheme: lightColorScheme,
+            useMaterial3: true,
+            // extensions: [lightCustomColors],
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkColorScheme,
+            useMaterial3: true,
+            // extensions: [darkCustomColors],
+          ),
+          home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
