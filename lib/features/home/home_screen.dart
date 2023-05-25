bebcb0cc41b631cbcdeaa39e1ed9b11212658caf
Shripal Jain <shripal.jain@leanix.net>
2023-05-25 22:19:01 +0200

    - Add home bloc, events, states - Add home screen (only structure) - Implement root checking in bloc (not integrated with screen yet)
    Signed-off-by: Shripal Jain <shripal.jain@leanix.net>

---
+++ lib/features/home/home_screen.dart
@@ -0,0 +1,73 @@
+import 'package:another_buddy/features/home/bloc/home_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+/*
+ * Created by Shripal Jain
+ * on 25/05/2023
+ */
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => HomeBloc(),
+      child: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double value = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          const SliverAppBar(
+            backgroundColor: Colors.white,
+            title: Text(
+              "Another Manager",
+              maxLines: 1,
+              softWrap: true,
+            ),
+            pinned: true,
+            floating: true,
+            snap: false,
+          )
+        ],
+        body: CustomScrollView(
+          slivers: [
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  const Text("CPU"),
+                  const SizedBox(height: 8),
+                  Slider(
+                    value: value,
+                    min: 0,
+                    max: 2,
+                    divisions: 2,
+                    onChanged: (newValue) => setState(() {
+                      value = newValue;
+                    }),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
